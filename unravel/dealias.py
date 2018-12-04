12bda903095743c508957922dea680255d71eb1a
Valentin Louf <v.louf@bom.gov.au>
2018-12-04 11:46:08 -0600

    Minor changes

--- unravel/dealias.py
+++ unravel/dealias.py
@@ -1,23 +1,11 @@
 """
 Driver script for the dealiasing module. You want to call process_3D.
 
-Even though most of the things here are new, it is still based (or inspired) by
-the works of:
- - J. Zhang and S. Wang, "An automated 2D multipass Doppler radar velocity
-   dealiasing scheme," J. Atmos. Ocean. Technol., vol. 23, no. 9, pp. 1239–1248,
-   2006.
- - G. He, G. Li, X. Zou, and P. S. Ray, "A velocity dealiasing scheme for
-   synthetic C-band data from China’s new generation weather radar system
-   (CINRAD)," J. Atmos. Ocean. Technol., vol. 29, no. 9, pp. 1263–1274, 2012.
- - G. Li, G. He, X. Zou, and P. S. Ray, "A velocity dealiasing scheme for
-   C-band weather radar systems," Adv. Atmos. Sci., vol. 31, no. 1, pp. 17–26,
-   2014.
-
 @title: dealias
 @author: Valentin Louf <valentin.louf@monash.edu>
 @institutions: Monash University and the Australian Bureau of Meteorology
 @creation: 05/04/2018
-@date: 04/10/2018
+@date: 04/12/2018
 
     count_proc
     dealiasing_process_2D

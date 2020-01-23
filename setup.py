30d75c257e860b8159cdf6bce0508c12d4a908ea
Valentin Louf <valentin.louf@bom.gov.au>
2020-01-23 15:16:00 +1100

    ADDED: Not compatible with numba 0.47.

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'numba', 'scipy'],  # Optional
+    install_requires=['numpy', 'numba<=0.46', 'scipy'],  # Optional
 
     # List additional URLs that are relevant to your project as a dict.
     #

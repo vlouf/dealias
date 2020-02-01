67595d2568bc5abbf3ed3b437a950f1bd4b81661
Valentin Louf <valentin.louf@bom.gov.au>
2020-02-01 21:33:01 +1100

    TODO: ODIM H5 File reader.

---
+++ unravel/odim.py
@@ -0,0 +1,13 @@
+"""
+ODIM convention file reader. Natively reads ODIM H5 files.
+
+@title: odim.py
+@author: Valentin Louf <valentin.louf@monash.edu>
+@institutions: Monash University and the Australian Bureau of Meteorology
+@date: 02/02/2020
+
+.. autosummary::
+    :toctree: generated/
+
+    
+"""

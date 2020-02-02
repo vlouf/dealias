040b1afdfa34036ce64fd1ffd86eddfe84ceb96f
Valentin Louf <valentin.louf@bom.gov.au>
2020-02-02 16:37:11 +1100

    REM odim reader to standalone odim module instead

--- setup.py
+++ setup.py
@@ -128,8 +128,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'numba<=0.46', 'scipy', 'xarray', 'pandas'],  # Optional
-    extras_require={"pyproj": ["odim"]},
+    install_requires=['numpy', 'numba<=0.46', 'scipy'],  # Optional    
 
     # List additional URLs that are relevant to your project as a dict.
     #

ed56a7bdb6d0b5afa023e9bf15b13e499183b78a
Valentin Louf <valentin.louf@bom.gov.au>
2020-02-25 16:00:34 +1100

    Update numba version

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'numba<=0.46'],  # Optional
+    install_requires=['numpy', 'numba!=0.47'],  # Optional
 
     # List additional URLs that are relevant to your project as a dict.
     #

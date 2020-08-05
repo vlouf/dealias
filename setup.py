bdc84d1f8e598f4cee4512711affee59e36caacd
Valentin Louf <valentin.louf@bom.gov.au>
2020-08-06 09:59:12 +1000

    Update dependencies

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'numba'],  # Optional
+    install_requires=['numpy', 'numba', 'astropy'],  # Optional
 
     # List additional URLs that are relevant to your project as a dict.
     #

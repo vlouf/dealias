da9c19e4b346f2bcc95e5e5987fdaf2f5ac42348
Valentin Louf <valentin.louf@bom.gov.au>
2020-12-15 13:03:03 +1100

    MODIF: update init

--- unravel/__init__.py
+++ unravel/__init__.py
@@ -1,1 +1,7 @@
-from .dealias import unravel_3D_pyart, unravel_3D_pyodim, dealias_long_range, dealiasing_process_2D
+from .dealias import (
+    unravel_3D_pyart,
+    unravel_3D_pyodim,
+    dealias_long_range,
+    dealiasing_process_2D,
+    unravel_3D_pyart_multiproc,
+)

a79c61222c19bdf4fde6399fdf38dc7dadab8c48
Valentin Louf <valentin.louf@bom.gov.au>
2021-03-15 16:07:29 +1100

    MODIF: update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.0',  # Required
+    version='1.2.5',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -104,6 +104,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     # This field adds keywords for your project which will appear on the
@@ -129,7 +130,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'numba', 'astropy'],  # Optional
+    install_requires=['numpy', 'numba'],  # Optional
 
     # List additional URLs that are relevant to your project as a dict.
     #

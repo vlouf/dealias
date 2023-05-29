5bf8525395966c8e4ed9a991c623ee23a2772093
Angus Montgomery <angus.montgomery@bom.gov.au>
2023-05-29 14:43:07 +1000

    nsum_moy explicit int in find_reference_radials()

--- unravel/find_reference.py
+++ unravel/find_reference.py
@@ -49,9 +49,12 @@ def find_reference_radials(azimuth, velocity):
     nsum_tot = np.sum(~np.isnan(velocity[nvalid_gate > 0, :]))
     nvalid_beam = len(azimuth[nvalid_gate > 0])
 
-    nsum_moy = nsum_tot / nvalid_beam
+    nsum_moy = nsum_tot // nvalid_beam
     if nsum_moy > 0.7 * velocity.shape[1]:
-        nsum_moy = 0.7 * velocity.shape[1]
+        nsum_moy = int(0.7 * velocity.shape[1])
+
+    if cfg().show_progress:
+        print(f"find_reference_radials vtotal:{nsum_tot} vbeams:{nvalid_beam} total_mean:{nsum_tot // nvalid_beam} beam_thresh:{nsum_moy} valid[0]:{nvalid_gate[0]} valid[1]:{nvalid_gate[1]}")
 
     try:
         start_beam = find_min_quadrant(azimuth, velocity, nvalid_gate, nsum_moy)

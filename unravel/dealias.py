524fcc37c53071812009205a67a1f2eed537e2ae
Angus Montgomery <angus.montgomery@bom.gov.au>
2023-12-08 11:10:10 +1100

    rename readwrite read_write

--- unravel/dealias.py
+++ unravel/dealias.py
@@ -500,7 +500,7 @@ def unravel_3D_pyodim(
     gatefilter=None,
     strategy="long_range",
     debug=False,
-    readwrite=False,
+    read_write=False,
     output_flag_name=None,
 ):
     """
@@ -524,7 +524,7 @@ def unravel_3D_pyodim(
         Feature not supported yet.
     strategy: ['default', 'long_range']
         Using the default dealiasing strategy or the long range strategy.
-    readwrite: write back to original file if True
+    read_write: write back to original file if True
 
     Returns:
     ========
@@ -546,9 +546,9 @@ def unravel_3D_pyodim(
 
     # NB: pyodim reader sorts tilts by elevation and time
     if load_all_fields:
-        (radar_datasets, h5file) = pyodim.read_write_odim(odim_file, readwrite=readwrite)
+        (radar_datasets, h5file) = pyodim.read_write_odim(odim_file, read_write=read_write)
     else:
-        (radar_datasets, h5file) = pyodim.read_write_odim(odim_file, readwrite=readwrite, include_fields=[vel_name])
+        (radar_datasets, h5file) = pyodim.read_write_odim(odim_file, read_write=read_write, include_fields=[vel_name])
     radar_datasets = [r.compute() for r in radar_datasets]
     # data_count = radar_datasets[0].attrs["data_count"]
 
@@ -588,7 +588,7 @@ def unravel_3D_pyodim(
             output_vel_name,
             output_flag_name)
 
-    if readwrite:
+    if read_write:
         for sweep in range(len(radar_datasets)):
             write_odim_slice(
                 h5file,

64ff3629a952e01e46996cad3ddc854086146cec
Angus Montgomery <angus.montgomery@bom.gov.au>
2024-05-17 13:35:20 +1000

    redo correct_range_backward consistent

--- unravel/continuity.py
+++ unravel/continuity.py
@@ -507,13 +507,9 @@ def correct_range_backward(vel, final_vel, flag_vel, vnyq, window_len=6, alpha=0
     if not cfg.DO_ACT:
         return final_vel, flag_vel
 
-    for nbeam in range(vel.shape[0]):
-        start_vec = np.where(flag_vel[nbeam, :] == 1)[0]
-        if len(start_vec) == 0:
-            continue
-
-        start_gate = start_vec[-1]
-        for ngate in np.arange(start_gate - (window_len + 1), window_len, -1):
+    maxazi, maxrange = vel.shape
+    for nbeam in range(maxazi):
+        for ngate in range(maxrange - 2, -1, -1):
             if flag_vel[nbeam, ngate] != 0:
                 continue
 

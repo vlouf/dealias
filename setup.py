4828a9037e999c8751b1564a4f05de15a214e10b
Valentin Louf <valentin.louf@bom.gov.au>
2025-05-31 11:48:07 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,30 +7,40 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+
+def parse_requirements(filename):
+    with open(filename, encoding="utf-8") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+
+REQUIRED = parse_requirements("requirements.txt")
 setup(
-    name="unravel",  # Required    
-    version="1.3.4",  # Required
+    name="unravel",  # Required
+    version="1.4.0",  # Required
+    author="Valentin Louf",  # Optional
+    author_email="valentin.louf@bom.gov.au",  # Optional
     description="A dealiasing technique for Doppler radar velocity.",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional (see note above)
     url="https://github.com/vlouf/dealias",  # Optional
-    author="Valentin Louf",  # Optional
-    author_email="valentin.louf@bom.gov.au",  # Optional
+    project_urls={
+        "Documentation": "https://github.com/vlouf/dealias#readme",
+        "Source": "https://github.com/vlouf/dealias",
+        "Tracker": "https://github.com/vlouf/dealias/issues",
+    },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Atmospheric Science",
-        "License :: OSI Approved :: MIT License",        
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     keywords="radar weather meteorology dealiasing Doppler",  # Optional
     packages=find_packages(exclude=["contrib", "docs", "tests"]),  # Required
-    install_requires=["numpy", "numba", "arm_pyart", "pyodim"],  # Optional
-    project_urls={  # Optional
-        "Bug Reports": "https://github.com/vlouf/dealias/issues",
-        "Source": "https://github.com/vlouf/dealias/",
-    },
+    install_requires=REQUIRED,
+    license="Apache-2.0",
+    python_requires=">=3.8",
 )
